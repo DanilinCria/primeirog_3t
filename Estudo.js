c4a6d467c52e30785a685574c4dceae309bc11ae
DanilinCria <110112709+DanilinCria@users.noreply.github.com>
2022-10-05 11:56:41 -0300

    Update Estudo.js

--- Estudo.js
+++ Estudo.js
@@ -1,8 +1,7 @@
 let tabuada = 5;
 
 function escreva(){
-    //2 x 1 = 2
-    document.write("Tabuada do " + tabuada + "<br>");
+    document.write("Tabuada do " + tabuada + "<br>")
     document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
     document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
     document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
@@ -13,5 +12,24 @@ function escreva(){
     document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
     document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
     document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
+}
 
+let lista = ["Terra","Marte","Saturno","Jupiter","Vênus",];
+
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    
+    for(let i = 0; i < 5; i++){
+        document.write("Planeta " + lista[i] + "<br>");
+
+    }
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+         document.write("Tabuada do " + i + "<br>");
+         for(let j = 1; j <=10; j++){
+           document.write(i+" x " + j + " = " +(i*j)+"<br>");
+    }
+    }
 }
